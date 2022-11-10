132c6468312a6d5949ad89938cd3efd2eedf454c
Nicky van Urk <nickyvanurk@gmx.com>
2022-11-10 21:42:47 +0100

    Add visitor pattern to AST generation script

--- jlox/src/lox/Expr.java
+++ jlox/src/lox/Expr.java
@@ -5,11 +5,22 @@ package lox;
 import java.util.List;
 
 abstract class Expr {
+    interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
     static class Binary extends Expr {
         Binary(Expr left, Token operator, Expr right) {
             this.left = left;
             this.operator = operator;
             this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
         }
 
         final Expr left;
@@ -21,11 +32,21 @@ abstract class Expr {
             this.expression = expression;
         }
 
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+
         final Expr expression;
     }
     static class Literal extends Expr {
         Literal(Object value) {
             this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
         }
 
         final Object value;
@@ -35,6 +56,13 @@ abstract class Expr {
             this.operator = operator;
         }
 
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+
         final Token operator;
     }
+
+    abstract <R> R accept(Visitor<R> visitor);
 }
--- jlox/src/tool/GenerateAst.java
+++ jlox/src/tool/GenerateAst.java
@@ -32,12 +32,17 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // The AST classes.
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
 
         writer.print("}");
         writer.close();
@@ -58,10 +63,29 @@ public class GenerateAst {
 
         writer.println("        }");
 
+        // Visitor pattern.
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
         // Fields.
         writer.println();
         for (String field : fields) {
             writer.println("        final " + field + ";");
+        }
+
+        writer.println("    }");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" + typeName + " " +
+                baseName.toLowerCase() + ");");
         }
 
         writer.println("    }");
