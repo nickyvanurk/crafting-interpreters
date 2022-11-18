2fdc41ce9a41f11c08bcef3d0ab954a897a1e776
Nicky van Urk <nickyvanurk@gmx.com>
2022-11-18 22:35:08 +0100

    Add interpreter class

---
+++ jlox/src/lox/Interpreter.java
@@ -0,0 +1,21 @@
+package lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        return new Object();
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return new Object();
+    }
+
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return new Object();
+    }
+
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        return new Object();
+    }
+}
