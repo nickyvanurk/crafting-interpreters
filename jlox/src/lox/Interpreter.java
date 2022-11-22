3bf154488796b46d96f07f3f0f55e437933ed1fb
Nicky van Urk <nickyvanurk@gmx.com>
2022-11-22 03:52:23 +0100

    Evaluate binary operators

--- jlox/src/lox/Interpreter.java
+++ jlox/src/lox/Interpreter.java
@@ -3,7 +3,42 @@ package lox;
 class Interpreter implements Expr.Visitor<Object> {
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
-        return new Object();
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESS:
+                return (double)left < (double)right;
+            case LESS_EQUAL:
+                return (double)left <= (double)right;
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+        }
+
+        // Unreachable.
+        return null;
     }
 
     @Override
@@ -33,6 +68,12 @@ class Interpreter implements Expr.Visitor<Object> {
         if (object == null) return false;
         if (object instanceof Boolean) return (boolean)object;
         return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+        return a.equals(b);
     }
 
     private Object evaluate(Expr expr) {
