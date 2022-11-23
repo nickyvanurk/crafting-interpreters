ad9a32fe97ff768130e94f2ad6351a53cd6dd578
Nicky van Urk <nickyvanurk@gmx.com>
2022-11-23 18:38:46 +0100

    Generate statement syntax tree

--- jlox/src/tool/GenerateAst.java
+++ jlox/src/tool/GenerateAst.java
@@ -11,12 +11,19 @@ public class GenerateAst {
             System.err.println("Usage: generate_ast <output directory>");
             System.exit(1);
         }
+
         String outputDir = args[0];
+
         defineAst(outputDir, "Expr", Arrays.asList(
             "Binary : Expr left, Token operator, Expr right",
             "Grouping : Expr expression",
             "Literal : Object value",
             "Unary : Token operator, Expr right"
+        ));
+
+        defineAst(outputDir, "Stmt", Arrays.asList(
+            "Expression : Expr expression",
+            "Print : Expr expression"
         ));
     }
 
