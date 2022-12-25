2e04c3155269aae99ebc18f3716d2451befddd25
Nicky van Urk <nickyvanurk@gmx.com>
2022-12-25 15:26:41 +0100

    Add logical operators

--- jlox/src/lox/Interpreter.java
+++ jlox/src/lox/Interpreter.java
@@ -67,10 +67,25 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return evaluate(expr.expression);
     }
 
+    @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
     }
 
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthy(left)) return left;
+        } else {
+            if (!isTruthy(left)) return left;
+        }
+
+        return evaluate(expr.right);
+    }
+
+    @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
 
